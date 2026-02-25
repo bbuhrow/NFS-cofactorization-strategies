cc3741847eed27132e9d438f3338e753fc16e869
buhrow <buhrow.benjamin@mayo.edu>
2026-02-25 08:59:46 -0600

    change default GCD bound back to max(lpba,lpbr) - 1

--- main.c
+++ main.c
@@ -575,7 +575,7 @@ int main(int argc, char **argv) {
 	if (batch_alg == 1)
 	{
 		char fname[80];
-		int file_bits = MAX(lpbr, lpba);
+		int file_bits = MAX(lpbr, lpba) - 1;
 		sprintf(fname, "bgcd_lpb%d", file_bits);
 		FILE* fid = fopen(fname, "rb");
 		int compute_pproduct = 1;
